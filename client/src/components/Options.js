959d88804fd56c5468cc0b08f34da139dbd883f4
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-26 15:57:08 +0900

    OrderContenxt 셋팅

--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -3,8 +3,7 @@ import React from "react";
 const Options = ({ name }) => {
   return (
     <form>
-      <input type="checkbox" id={`${name} option`} />
-      <label htmlFor={`${name} option`}>{name}</label>
+      <input type="checkbox" id={`${name} option`} /> <label htmlFor={`${name} option`}>{name}</label>
     </form>
   );
 };
