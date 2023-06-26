ad257cd11933d5bde876c1dc906ebc5ef2f4310c
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-26 14:49:02 +0900

    Type, Options, Products Component작성 및 OrderPage 작성

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SummaryPage from "./pages/SummaryPage";
+import OrderPage from "./pages/OrderPage";
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ padding: "4rem" }}>
+      <OrderPage />
       <SummaryPage />
     </div>
   );
